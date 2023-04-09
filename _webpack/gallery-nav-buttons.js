3bb057e490eabad40c8e707aecb9d8db1812db14
Fausto M. Lagos S <fausto@autonomia.digital>
2023-04-09 00:27:25 -0500

    Added complete capacity of navigation by gallery (serie) and picture.

--- _webpack/gallery-nav-buttons.js
+++ _webpack/gallery-nav-buttons.js
@@ -1,27 +1,192 @@
+const contentGalleries = {
+    coltab2014: {
+        title: "Serie COL TAB 2014 IMG XIAO",
+        coltab201401: {
+            title: "COL-TAB-01",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL_TAB-2014-01.jpg"
+        },
+        coltab201402: {
+            title: "COL-TAB-02",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL_TAB-2014-02.jpg"
+        },
+        coltab201403: {
+            title: "COL-TAB-03",
+            description: "Acrylic, size 51,18/114,56 in. 130/291 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-03.jpg"
+        },
+        coltab201404: {
+            title: "COL-TAB-04",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-04.jpg"
+        },
+        coltab201405: {
+            title: "COL-TAB-05",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-05.jpg"
+        },
+        coltab201406: {
+            title: "COL-TAB-06",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-06.jpg"
+        },
+        coltab201407: {
+            title: "COL-TAB-07",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-07.jpg"
+        },
+        coltab201408: {
+            title: "COL-TAB-08",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-08.jpg"
+        },
+        coltab201409: {
+            title: "COL-TAB-09",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-09.jpg"
+        },
+        coltab2014010: {
+            title: "COL-TAB-010",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-010.jpg"
+        },
+        coltab2014011: {
+            title: "COL-TAB-011",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-011.jpg"
+        },
+        coltab2014012: {
+            title: "COL-TAB-012",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-012.jpg"
+        },
+        coltab2014013: {
+            title: "COL-TAB-013",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-013.jpg"
+        },
+        coltab2014014: {
+            title: "COL-TAB-014",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-014.jpg"
+        },
+        coltab2014015: {
+            title: "COL-TAB-015",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-015.jpg"
+        },
+        coltab2014016: {
+            title: "COL-TAB-016",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-016.jpg"
+        },
+        coltab2014017: {
+            title: "COL-TAB-017",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-017.jpg"
+        },
+        coltab2014018: {
+            title: "COL-TAB-018",
+            description: "Acrylic, size 102,36/230,32 in. 260/585 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-018.jpg"
+        },
+        coltab2014019: {
+            title: "COL-TAB-019",
+            description: "Acrylic, size 102,36/383,86 in. 260/975 cm.",
+            photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL-TAB-2014-019.jpg"
+        }
+    },
+    espatab: {
+        title: "Serie ESPA TAB IMG XIAO",
+        espatab01: {
+            title: "ESPA TAB 01",
+            description: "Acrylic, size 76.77/102.36 inch 195/260 cm.",
+            photo: "/assets/images/galleries/SERIE_ESPA_TAB_IMG_XIAO/ESPA_Tab_01.jpg"
+        },
+        espatab02: {
+            title: "ESPA TAB 02",
+            description: "Acrylic, size 51.18/38.19 inch 130/97 cm.",
+            photo: "/assets/images/galleries/SERIE_ESPA_TAB_IMG_XIAO/ESPA_Tab_02.jpg"
+        }
+    },
+    eventInorg: {
+        title: "Serie EVENTOS INORGÁNICOS XIAO",
+        eventInorg01: {
+            title: "EVENTO INORGÁNICO 01",
+            description: "Acrilic on geotextil, size 102.36/76.77 inch 260/195 cm.",
+            photo: "/assets/images/galleries/SERIE_EVENTOS_INORGANICOS_XIAO/EVENTO_INORGANICO_01.jpg"
+        },
+        eventInorg02: {
+            title: "EVENTO INORGÁNICO 02",
+            description: "Acrilic on geotextil, size 102.36/76.77 inch 260/195 cm.",
+            photo: "/assets/images/galleries/SERIE_EVENTOS_INORGANICOS_XIAO/EVENTO_INORGANICO_02.jpg"
+        }
+    },
+    hktab: {
+        title: "Serie HONG KONG 89 IMG XIAO",
+        hktab02: {
+            title: "HK TAB 89 02",
+            description: "Papel de arroz size 62.99/39.27 inch 160/100 cm.",
+            photo: "/assets/images/galleries/SERIE_HK_TAB_89_IMG_XIAO/HK-TAB-89-02.jpg"
+        },
+        hktab03: {
+            title: "HK TAB 89 03",
+            description: "Papel de arroz size 62.99/39.27 inch 160/100 cm.",
+            photo: "/assets/images/galleries/SERIE_HK_TAB_89_IMG_XIAO/HK-TAB-89-03.jpg"
+        }
+    },
+    parisNoire: {
+        title: "Serie PARIS NOIRE 99 IMG XIAO",
+        parisNoire01: {
+            title: "PARIS NOIRE 99 01",
+            description: "Acrilyc size 102.36/76.77 inch 260/195 cm.",
+            photo: "/assets/images/galleries/SERIE_PARIS_NOIRE_99_IMG_XIAO/Paris_Noire_99_01.jpg"
+        },
+        parisNoire02: {
+            title: "PARIS NOIRE 99 02",
+            description: "Acrilyc size 102.36/76.77 inch 260/195 cm.",
+            photo: "/assets/images/galleries/SERIE_PARIS_NOIRE_99_IMG_XIAO/Paris_Noire_99_02.jpg"
+        }
+    }
+};
+
 const backwardGalleryButton = document.getElementById("backward-gallery");
 const forwardGalleryButton = document.getElementById("forward-gallery");
 const backwardPictureButton = document.getElementById("backward-picture");
 const forwardPictureButton = document.getElementById("forward-picture");
+const galleryTitle = document.getElementById('gallery-title');
+const picture = document.getElementById('picture');
+const pictureDetails = document.getElementById('picture-description');
 
-backwardGalleryButton.addEventListener("click", selectGallery);
-forwardGalleryButton.addEventListener("click", selectGallery);
+const galleries = Object.keys(contentGalleries);
+let currentGallery = {};
+let picturesInCurrentGallery = [];
+const setDefaults = () => {
+    const defaultGallery = galleries[0];
+    picturesInCurrentGallery = Object.keys(contentGalleries[defaultGallery]).slice(1);
+    const defaultPicture = picturesInCurrentGallery[0];
+    currentGallery = contentGalleries[defaultGallery];
+    galleryTitle.innerHTML = currentGallery.title;
+    picture.src = contentGalleries[defaultGallery][defaultPicture].photo;
+    pictureDetails.innerHTML = contentGalleries[defaultGallery][defaultPicture].title + ": " + contentGalleries[defaultGallery][defaultPicture].description;
+};
 
-const galleries = [
-    "COL-TAB-2014",
-    "ESPA-TAB",
-    "EVENTOS INORGÁNICOS",
-    "HK TAB",
-    "PARIS NOIRE",
-    "PARIS TAB 91",
-    "PARIS TAB 95",
-    "PARIS TAB 99",
-    "PEQUÍN 87",
-    "SHANSHUEI HUA",
-];
-
+setDefaults();
+backwardGalleryButton.addEventListener("click", function(){selectGallery('backward')});
+forwardGalleryButton.addEventListener("click", function() {selectGallery('forward')});
 let currentGalleryIndex = 0;
+
+const setCurrents = (selectedGallery) => {
+    currentGallery = selectedGallery;
+    picturesInCurrentGallery = Object.keys(currentGallery).slice(1);
+    const currentPicture = picturesInCurrentGallery[0];
+    galleryTitle.innerHTML = currentGallery.title;
+    picture.src = currentGallery[currentPicture].photo;
+    pictureDetails.innerHTML = currentGallery[currentPicture].title + ": " + currentGallery[currentPicture].description;
+}
 function selectGallery(direction) {
-    if (direction === 'back'){
+    if (direction === 'backward'){
         if (currentGalleryIndex <= 0) {
             currentGalleryIndex = galleries.length - 1;
         } else {
@@ -35,24 +200,28 @@ function selectGallery(direction) {
         }
     }
 
-    document.getElementById('gallery-title').innerHTML = galleries[currentGalleryIndex];
+    const currentGallery = contentGalleries[galleries[currentGalleryIndex]];
+    setCurrents(currentGallery);
 }
 
-const colTab2014 = {
-    COLTAB01: {
-        title: "COL-TAB-01",
-        description: "Acrylic, size 102,36 / 230,32 in. 260/585 cm.",
-        photo: "/assets/images/galleries/SERIE_COL_TAB_2014_IMG_XIAO/COL_TAB-2014-01.jpg"
-    },
-    COLTAB02: {
-        title: "COL-TAB-02",
-        description: "Acrylic, size 102,36 / 230,32 in. 260/585 cm.",
-        photo: "/assets/images/galleries/COL-TAB-2014-02.jpg"
+backwardPictureButton.addEventListener("click", function () {selectPicture('backward')});
+forwardPictureButton.addEventListener("click", function () {selectPicture('forward')});
+let currentPictureIndex = 0;
+function selectPicture(direction) {
+    if (direction === 'backward'){
+        if (currentPictureIndex <= 0) {
+            currentPictureIndex = picturesInCurrentGallery.length - 1;
+        } else {
+            currentPictureIndex -= 1;
+        }
+    } else {
+        if (currentPictureIndex >= picturesInCurrentGallery.length-1) {
+            currentPictureIndex = 0;
+        } else {
+            currentPictureIndex += 1;
+        }
     }
-};
 
-let gallery = colTab2014;
-function selectPicture() {
-    document.getElementById('picture').src = gallery.COLTAB01.photo;
+    picture.src = currentGallery[picturesInCurrentGallery[currentPictureIndex]].photo;
+    pictureDetails.innerHTML = currentGallery[picturesInCurrentGallery[currentPictureIndex]].title + ": " + currentGallery[picturesInCurrentGallery[currentPictureIndex]].description;
 }
-backwardPictureButton.addEventListener("click", selectPicture);
